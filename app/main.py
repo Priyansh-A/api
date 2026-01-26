7664f733dc0aa5aea59ccf9ba73544a3847d0aad
Priyanshu Awasthi <nppriyansh112@gmail.com>
2026-01-26 10:09:40 +0545

    cors

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from  .database import  create_db_and_tables
 from contextlib import asynccontextmanager
 from .routers import post, user, auth, like
-
+from fastapi.middleware.cors import CORSMiddleware
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -13,6 +13,23 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "https://www.google.com"
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins= origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(post.router)
 app.include_router(user.router)
